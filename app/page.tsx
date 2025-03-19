6e79f8490d88791449453701d484a812b0abd37f
jakottelaar <jarno.kottelaar@gmail.com>
2025-03-19 19:53:41 +0100

    Setup new project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      Page
+    </div>
+  );
+};
+
+export default Page;
